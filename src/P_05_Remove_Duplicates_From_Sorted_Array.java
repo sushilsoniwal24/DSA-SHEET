58056ea4610f77987c23dfa29a2403c9e47815c7
Sushil Soniwal <96940687+sushilsoniwal24@users.noreply.github.com>
2023-02-28 09:19:34 +0530

    Remove Duplicates From Sorted Array

---
+++ src/P_05_Remove_Duplicates_From_Sorted_Array.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+
+public class P_05_Remove_Duplicates_From_Sorted_Array {
+
+    //    https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
+
+    // T.C = O(n) & S.C = O(n).
+    public static int removeDuplicates1(int[] nums) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int num : nums) {
+            if (!list.contains(num)) {
+                list.add(num);
+            }
+        }
+
+        int idx = 0;
+        for (Integer integer : list) {
+            nums[idx++] = integer;
+        }
+        return list.size();
+    }
+
+    // T.C = O(n) & S.C = O(1).
+    public static int removeDuplicates2(int[] nums) {
+        int length = nums.length;
+        int currentIndex = 0, nextIndex = 1;
+
+        while (nextIndex < length) {
+            if (nums[currentIndex] != nums[nextIndex]) {
+                nums[++currentIndex] = nums[nextIndex];
+            }
+            nextIndex++;
+        }
+        return currentIndex + 1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+        System.out.println(removeDuplicates2(arr));
+    }
+}
